ad4ff3ab5008cfd81abb59669fa7c11c7dddc193
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-08-06 12:42:00 +0100

    finally,
    there's a secure version

--- audio.js
+++ audio.js
@@ -92,7 +92,7 @@ audioPlayerInteraction = {
     varz.playAnimation = bodymovin.loadAnimation({
         container: varz.playIcon,
         path: 'pause.json', //for production
-        // path: 'http://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
+        // path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
         renderer: 'svg',
         loop: false,
         autoplay: false
